155e0738ffaa5d4d7d125651669bfc75f5cb4560
Patryk Karasiński <patryk.karasinski@treesat.io>
2024-08-29 13:40:51 +0200

    Add ThemeProvider; Add 'register' route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {
   redirect,
   RouterProvider,
 } from 'react-router-dom';
+import {createTheme, ThemeProvider} from '@mui/material';
+import {deepOrange} from '@mui/material/colors';
 
 import Navigation from './components/Navigation/Navigation.tsx';
 import Home from './pages/Home/Home.tsx';
@@ -12,6 +14,7 @@ import Logout from './pages/Logout/Logout.tsx';
 import Products from './pages/Products/Products.tsx';
 import Profile from './pages/Profile/Profile.tsx';
 import Recipes from './pages/Recipes/Recipes.tsx';
+import Register from './pages/Register/Register.tsx';
 
 import {useUserStore} from './store/userStore.ts';
 
@@ -44,6 +47,10 @@ const router = createBrowserRouter([
         Component: Logout,
       },
       {
+        path: 'register',
+        Component: Register,
+      },
+      {
         path: 'profile',
         loader: protectedLoader,
         Component: Profile,
@@ -64,14 +71,20 @@ function App() {
   return <RouterProvider router={router} />;
 }
 
+const theme = createTheme({
+  palette: {
+    primary: deepOrange,
+  },
+});
+
 function Layout() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Navigation />
       <main>
         <Outlet />
       </main>
-    </>
+    </ThemeProvider>
   );
 }
 
