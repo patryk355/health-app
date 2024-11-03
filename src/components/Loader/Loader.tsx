5501c46e339ec4e587e7d3362c82ec676df7c40a
Patryk Karasiński <patryk.karasinski@treesat.io>
2024-11-03 19:58:03 +0100

    Center Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,17 +2,19 @@ import {CircularProgress} from '@mui/material';
 
 const Loader = () => {
   return (
-    <>
+    <div
+      style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+    >
       <svg width={0} height={0}>
         <defs>
           <linearGradient id='gradient' x1='0%' y1='0%' x2='0%' y2='100%'>
-            <stop offset='0%' stopColor='#E01CD5' />
-            <stop offset='100%' stopColor='#1CB5E0' />
+            <stop offset='0%' stopColor='#ff5722' />
+            <stop offset='100%' stopColor='#d32f2f' />
           </linearGradient>
         </defs>
       </svg>
       <CircularProgress sx={{'svg circle': {stroke: 'url(#gradient)'}}} />
-    </>
+    </div>
   );
 };
 
