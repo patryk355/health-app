36fb7116d466be334403e0423268daa5da4b8bbc
Patryk Karasiński <patryk.karasinski@treesat.io>
2024-11-03 19:57:23 +0100

    Add imageValidator function

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,3 +1,15 @@
 export const emailValidator = (value: string) => {
   return !!value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
 };
+
+export const imageValidator = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const contentType = response.headers.get('Content-Type');
+
+    return !!(contentType && contentType.startsWith('image'));
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
