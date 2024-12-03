eef0abe112e9d0e69a70209c214591441882271e
Patryk Karasiński <patryk.karasinski@treesat.io>
2024-12-03 22:18:56 +0100

    Create Profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,27 @@
+import {useTranslation} from 'react-i18next';
+import {useUserStore} from '../../store/userStore.ts';
+
+import styles from './Profile.module.scss';
+
 const Profile = () => {
+  const {t} = useTranslation(['users', 'common']);
+
+  const user = useUserStore((state) => state.user);
+
   return (
-    <div id='profile'>
-      <h1>Profile</h1>
+    <div className={styles.container}>
+      <dl>
+        <dt>{t('common:USERNAME')}</dt>
+        <dd>{user?.username}</dd>
+      </dl>
+      <dl>
+        <dt>{t('common:EMAIL')}</dt>
+        <dd>{user?.email}</dd>
+      </dl>
+      <dl>
+        <dt>{t('ROLE')}</dt>
+        <dd>{user?.role ? t(user.role) : t('UNKNOWN')}</dd>
+      </dl>
     </div>
   );
 };
